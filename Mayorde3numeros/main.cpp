502693b3944f36406dca19fd3d629089d35e93cd
ALUMNO-G <ALUMNO-G@UCSP.AULAB>
2019-08-23 09:15:28 -0500

    last points

--- Mayorde3numeros/main.cpp
+++ Mayorde3numeros/main.cpp
@@ -3,21 +3,21 @@ using namespace std;
 int main()
 {
     int A,B,C;
-    cout<<("Ingresa un numero ")<<endl;
+    cout<<("Ingresa un numero : ")<<endl;
     cin>>A;
-    cout<<("Ingresa otro numero  ")<<endl;
+    cout<<("Ingresa otro numero : ")<<endl;
     cin>>B;
-    cout<<("Ingresa un numero  ")<<endl;
+    cout<<("Ingresa un numero : ")<<endl;
     cin>>C;
     if(A > B && A > C)
     {
-    cout<<"El numero mayor es "<<A;
+    cout<<"El numero mayor es : "<<A;
     }
     else
     {
         if(B > A && B > C)
         {
-        cout<<"El numero mayor es "<<B;
+        cout<<"El numero mayor es : "<<B;
         }else
         {
         cout<<"El numero mayor es "<<C;
